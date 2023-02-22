1689030a3ede8faf6904b4278e3240f2dea6e9ed
TCHAKAL <ahmedtchakal@gmail.com>
2023-02-22 14:25:09 +0100

    les composants web (model + list)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Pokemon} from "./models/pokemon";
+import {POKEMONS} from "./models/mock-pokemon-list";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon-go';
+  pokemonsList: Pokemon[] = POKEMONS;
+
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.table(this.pokemonsList)
+    this.selectPokemon(POKEMONS[2]);
+
+  }
+
+  selectPokemon(pokemon: Pokemon): void {
+    console.log(`vous avez cliquer sur le pokemon ${pokemon.name}`);
+  }
 }
