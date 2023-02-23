333b8345dcc0f323a931fd4d0e217b7f3e478cfc
TCHAKAL <ahmedtchakal@gmail.com>
2023-02-23 11:59:26 +0100

    paramétrer une directive d'attribut

--- src/app/directives/border-card.directive.ts
+++ src/app/directives/border-card.directive.ts
@@ -1,17 +1,19 @@
-import {Directive, ElementRef, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[pokemonBorderCard]'
 })
 export class BorderCardDirective {
-
   constructor(private el: ElementRef) {
     this.setHeight(180);
     this.setBorder('#f5f5f5')
   }
 
+  @Input('pokemonBorderCard') borderColor: string;//alias
+  //@Input() pokemonBorderCard: string;//sans alias
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder('#009688');
+    this.setBorder(this.borderColor || '#009688');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
