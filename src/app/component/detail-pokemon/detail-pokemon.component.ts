115475ee3ea51f68f96700d8e9ed57ec802eb424
TCHAKAL <ahmedtchakal@gmail.com>
2023-02-23 15:36:16 +0100

    Routage entre détail et list

--- src/app/component/detail-pokemon/detail-pokemon.component.ts
+++ src/app/component/detail-pokemon/detail-pokemon.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Pokemon} from "../../models/pokemon";
+import {POKEMONS} from "../../models/mock-pokemon-list";
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
   styleUrls: ['./detail-pokemon.component.scss']
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit {
+
+  pokemonList: Pokemon[];
+  currentPokemon: Pokemon | undefined;
+
+  constructor(private router: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null = this.router.snapshot.paramMap.get('id');
+    if (pokemonId) {
+      this.currentPokemon = this.pokemonList.find(pokemon => +pokemonId == pokemon.id);
+    }
+  }
 
 }
