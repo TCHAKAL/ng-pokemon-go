84cefcc6886634ffee598fae846c99111d47150c
TCHAKAL <ahmedtchakal@gmail.com>
2023-02-26 15:03:59 +0100

    Mise en place du FormsModule

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -6,6 +6,7 @@ import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
 import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
 import {RouterModule, Routes} from "@angular/router";
 import {PokemonService} from "./services/pokemon.service";
+import {FormsModule} from "@angular/forms";
 
 const pokemonRoutes: Routes = [
   {path: 'pokemons', component: ListPokemonComponent},
@@ -21,6 +22,7 @@ const pokemonRoutes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(pokemonRoutes)
   ],
   providers: [
