8d96c31992f2ad24e6a3bbed588fd2700916ad3c
TCHAKAL <ahmedtchakal@gmail.com>
2023-03-14 10:41:20 +0100

    Ajouter un Guard de type CanActivate

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -10,14 +10,15 @@ import {FormsModule} from "@angular/forms";
 import {PokemonFormComponent} from "./pokemon-form/pokemon-form.component";
 import {EditPokemonComponent} from './edit-pokemon/edit-pokemon.component';
 import {AddPokemonComponent} from './add-pokemon/add-pokemon.component';
-import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
-import { LoaderComponent } from './loader/loader.component';
+import {SearchPokemonComponent} from './search-pokemon/search-pokemon.component';
+import {LoaderComponent} from './loader/loader.component';
+import {AuthGuard} from "../auth.guard";
 
 const pokemonRoutes: Routes = [
-  {path: 'edit/pokemon/:id', component: EditPokemonComponent},
-  {path: 'add/pokemon', component: AddPokemonComponent},
-  {path: 'pokemons', component: ListPokemonComponent},
-  {path: 'pokemons/:id', component: DetailPokemonComponent},
+  {path: 'edit/pokemon/:id', component: EditPokemonComponent, canActivate: [AuthGuard]},
+  {path: 'add/pokemon', component: AddPokemonComponent, canActivate: [AuthGuard]},
+  {path: 'pokemons', component: ListPokemonComponent, canActivate: [AuthGuard]},
+  {path: 'pokemons/:id', component: DetailPokemonComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
