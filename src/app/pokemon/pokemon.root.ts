9813633977bbeee114e610e58d7ae3adebc278b6
TCHAKAL <ahmedtchakal@gmail.com>
2023-06-10 12:28:30 +0200

    Ajouter des titres dans les pages

--- src/app/pokemon/pokemon.root.ts
+++ src/app/pokemon/pokemon.root.ts
@@ -8,18 +8,22 @@ export default [{
   children: [
     {
       path: 'edit/pokemon/:id',
+      title: 'Modifier un Pokemon',
       loadComponent: () => import('./edit-pokemon/edit-pokemon.component').then(editPokemon => editPokemon.EditPokemonComponent)
     },
     {
       path: 'add/pokemon',
+      title: 'Ajouter un Pokemon',
       component: AddPokemonComponent,
     },
     {
       path: 'pokemons',
+      title: 'Liste des Pokemons',
       loadComponent: () => import('./list-pokemon/list-pokemon.component').then(listPokemon => listPokemon.ListPokemonComponent),
     },
     {
       path: 'pokemons/:id',
+      title: 'Consulter un Pokemon',
       loadComponent: () => import('./detail-pokemon/detail-pokemon.component').then(detailPokemon => detailPokemon.DetailPokemonComponent),
     },
   ]
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,43 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { InMemoryDataService } from './app/in-memory-data.service';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { PokemonService } from './app/pokemon/services/pokemon.service';
-import { AuthGuard } from './app/auth.guard';
-import { provideRouter, Routes } from '@angular/router';
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {InMemoryDataService} from './app/in-memory-data.service';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {PokemonService} from './app/pokemon/services/pokemon.service';
+import {AuthGuard} from './app/auth.guard';
+import {provideRouter, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'pokemons',
-        pathMatch: 'full',
-    },
-    {
-        path: '',
-        loadChildren: () => import("./app/pokemon/pokemon.root"),
-        canActivate: [AuthGuard],
-        providers: [PokemonService],
-    },
-    {
-        path: 'login',
-        loadComponent: () => import("./app/login/login.component").then(module => module.LoginComponent),
-    },
-    {
-        path: '**',
-        loadComponent: () => import("./app/page-not-found/page-not-found.component").then(module => module.PageNotFoundComponent),
-    }
+  {
+    path: '',
+    redirectTo: 'pokemons',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    loadChildren: () => import("./app/pokemon/pokemon.root"),
+    canActivate: [AuthGuard],
+    providers: [PokemonService],
+  },
+  {
+    path: 'login',
+    title: 'Page de connexion',
+    loadComponent: () => import("./app/login/login.component").then(module => module.LoginComponent),
+  },
+  {
+    path: '**',
+    title: 'Page non trouvée',
+    loadComponent: () => import("./app/page-not-found/page-not-found.component").then(module => module.PageNotFoundComponent),
+  },
 ];
 
-
-
 bootstrapApplication(AppComponent, {
-    providers: [
-      provideHttpClient(withInterceptorsFromDi()),
-      importProvidersFrom(BrowserModule, FormsModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })),
-      provideRouter(routes)
-    ]
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(BrowserModule, FormsModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})),
+    provideRouter(routes)
+  ]
 })
   .catch(err => console.error(err));
