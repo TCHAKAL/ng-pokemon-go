4d76fc05f00fd26fdf4ce015d9176dd884a25460
TCHAKAL <ahmedtchakal@gmail.com>
2023-06-10 12:37:43 +0200

    Ajouter des titres avec le service Title d'angular

--- src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
+++ src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
@@ -2,15 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {PokemonService} from "../services/pokemon.service";
 import {Pokemon} from "../models/pokemon";
-import { PokemonFormComponent } from '../pokemon-form/pokemon-form.component';
-import { NgIf } from '@angular/common';
+import {PokemonFormComponent} from '../pokemon-form/pokemon-form.component';
+import {NgIf} from '@angular/common';
+import {Title} from "@angular/platform-browser";
 
 @Component({
-    selector: 'app-edit-pokemon',
-    templateUrl: './edit-pokemon.component.html',
-    styleUrls: ['./edit-pokemon.component.scss'],
-    standalone: true,
-    imports: [NgIf, PokemonFormComponent]
+  selector: 'app-edit-pokemon',
+  templateUrl: './edit-pokemon.component.html',
+  styleUrls: ['./edit-pokemon.component.scss'],
+  standalone: true,
+  imports: [NgIf, PokemonFormComponent]
 })
 export class EditPokemonComponent implements OnInit {
 
@@ -19,15 +20,26 @@ export class EditPokemonComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private pokemonService: PokemonService,
+    private title: Title,
   ) {
   }
 
   ngOnInit(): void {
     const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
     if (pokemonId) {
-      this.pokemonService.getPokemonById(+pokemonId).subscribe(pokemon => this.pokemon = pokemon);
+      this.pokemonService
+        .getPokemonById(+pokemonId)
+        .subscribe(pokemon => this.pokemon = pokemon);
     } else {
       this.pokemon = undefined;
+    }
+  }
+
+  initTitle(pokemon: Pokemon | undefined) {
+    if (!pokemon) {
+      this.title.setTitle('Pokemon not found');
+    } else {
+      this.title.setTitle('Modifier ' + pokemon.name)
     }
   }
 
